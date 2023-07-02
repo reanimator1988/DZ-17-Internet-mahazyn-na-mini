c01234bfe5f642e1d1c6f760c25033388e930247
DiMa <francev.dima@gmail.com>
2023-07-02 20:55:10 +0200

    dz 1

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+document.querySelector('.root-nav').onclick = function (event) {
+    console.log(event.target.nextElementSibling);
+    if (event.target.nodeName !== 'SPAN') return;
+    closeAllSubMenu(event.target.nextElementSibling);
+    event.target.nextElementSibling.classList.toggle('sub-menu-active');
+}
+
+function closeAllSubMenu(current = null) {
+    let parents = [];
+    if (current) {
+        let currentParent = current.parentNode;
+
+        while (currentParent) {
+            if (currentParent.classList.contains('root-nav')) break;
+            if (currentParent.nodeName === 'UL') parents.push(currentParent);
+            currentParent = currentParent.parentNode;
+        }
+    }
+
+    const subMenu = document.querySelectorAll('.root-nav ul');
+    Array.from(subMenu).forEach(item => {
+        if (item != current && !parents.includes(item)) {
+            item.style.display = "none";
+            item.classList.remove('sub-menu-active');
+        } else {
+            item.style.display = "block";
+        }
+    });
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener("click", kupiti);
+});
+
+function kupiti(event) {
+  alert("Товар куплен");
+  document.querySelectorAll('.root-nav ul').forEach(function (li) {
+    if (!event.target.classList.contains('sub-menu-active') && li.style.display !== "none") {
+      li.style.display = "none";
+    }
+  });
+}
+
